38a452823e2689610f6f99a943fb9e72a11f574a
Marcus Rossel <marcusrossel@icloud.com>
2025-05-05 19:23:15 +0200

    Use `List.intersperse`

--- HumanEvalLean/HumanEval5.lean
+++ HumanEvalLean/HumanEval5.lean
@@ -1,27 +1,27 @@
-def intersperse : List Int → Int → List Int
-  | [],             _     => []
-  | [i],            _     => [i]
-  | i₁ :: i₂ :: tl, delim => i₁ :: delim :: intersperse (i₂ :: tl) delim
+example : [].intersperse 7 = []                              := rfl
+example : [5, 6, 3, 2].intersperse 8 = [5, 8, 6, 8, 3, 8, 2] := rfl
+example : [2, 2, 2].intersperse 2 = [2, 2, 2, 2, 2]          := rfl
 
-example : intersperse [] 7 = []                              := rfl
-example : intersperse [5, 6, 3, 2] 8 = [5, 8, 6, 8, 3, 8, 2] := rfl
-example : intersperse [2, 2, 2] 2 = [2, 2, 2, 2, 2]          := rfl
+namespace List
 
-theorem intersperse_length_le : (intersperse nums delim).length ≤ 2 * nums.length - 1 := by
-  fun_induction intersperse <;> simp only [intersperse, List.length_cons] at * <;> omega
+theorem intersperse_length_le (l : List α) : (l.intersperse delim).length ≤ 2 * l.length - 1 := by
+  fun_induction intersperse <;> simp only [intersperse, length_cons] at * <;> try omega
+  next h _ =>
+    have := length_pos_iff.mpr h
+    omega
 
 -- Every element of index `2 * i` is the `i`th element of the input list.
-theorem intersperse_get?_num (h : 1 < nums.length) :
-    (intersperse nums delim)[2 * i]? = nums[i]? := by
+theorem intersperse_getElem?_even {l : List α} (h : 1 < l.length) :
+    (l.intersperse delim)[2 * i]? = l[i]? := by
   fun_induction intersperse generalizing i <;> try contradiction
-  next tl _ ih =>
+  next hn ih =>
+    have ⟨_, tl, hn⟩ := ne_nil_iff_exists_cons.mp hn
     cases tl <;> cases i
     case nil.succ j =>
-      cases j <;> simp +arith [intersperse]
+      cases j <;> simp +arith [hn, intersperse]
     case cons.succ j =>
       have hj : 2 * (j + 1) = 2 * j + 2 := rfl
-      rw [intersperse, hj]
-      simp [ih]
+      rw [intersperse, hj] <;> simp_all
     all_goals simp [intersperse]
 
 /-!
