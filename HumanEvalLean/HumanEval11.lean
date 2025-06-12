3660e31b9ef5469081955b15703b89a08644d8e7
Markus Himmel <markus@lean-fro.org>
2025-06-12 06:32:54 +0200

    Add solution for HumanEval11

--- HumanEvalLean/HumanEval11.lean
+++ HumanEvalLean/HumanEval11.lean
@@ -1,5 +1,17 @@
-def string_xor : Unit :=
-  ()
+import Std.Data.Iterators
+
+def stringXor (a b : List Bool) : List Bool :=
+  ((a.iter).zip b.iter)
+    |>.map (fun p => Bool.xor p.1 p.2)
+    |>.toList
+
+@[simp, grind]
+theorem length_stringXor {a b : List Bool} : (stringXor a b).length = min a.length b.length := by
+  simp [stringXor]
+
+theorem getElem_stringXor {a b : List Bool} {i : Nat} {hia : i < a.length} {hib : i < b.length} :
+    (stringXor a b)[i]'(by grind) = Bool.xor a[i] b[i] := by
+  simp [stringXor]
 
 /-!
 ## Prompt
