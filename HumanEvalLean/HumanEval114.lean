e5e2c069f5d21d8057aa96764b9182c2436cbfea
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-01-02 10:26:20 +0100

    wip

--- HumanEvalLean/HumanEval114.lean
+++ HumanEvalLean/HumanEval114.lean
@@ -1,5 +1,83 @@
-def minSubArraySum : Unit :=
-  ()
+import Std.Tactic.Do
+open Std.Do
+
+#check List.min
+
+def Array.minD [Min α] (xs : Array α) (fallback : α) : α :=
+    xs.toList.min?.getD fallback
+
+attribute [grind =] Array.toList_mkSlice_rco Array.toList_mkSlice_rci
+
+@[simp, grind =]
+theorem Array.minD_empty [Min α] {fallback : α} :
+    (#[] : Array α).minD fallback = fallback := by
+  simp [Array.minD]
+
+def minSubarraySum (xs : Array Int) : Int := Id.run do
+    let mut minSum := 0
+    let mut s := 0
+    for num in xs do
+        s := min 0 (s + num)
+        minSum := min s minSum
+    if minSum < 0 then
+        return minSum
+    else
+        return xs.minD 0
+
+@[simp, grind =]
+theorem minSubarraySum_empty : minSubarraySum #[] = 0 := by simp [minSubarraySum]
+
+def IsMinSubarraySum₀ (xs : Array Int) (s : Int) : Prop :=
+  (∃ (i j : Nat), i ≤ j ∧ j ≤ xs.size ∧ s = xs[i...j].toList.sum) ∧
+    (∀ (i j : Nat), i ≤ j → j ≤ xs.size → s ≤ xs[i...j].toList.sum)
+
+def IsMinSuffixSum₀ (xs : Array Int) (s : Int) : Prop :=
+  (∃ (i : Nat), i ≤ xs.size ∧ s = xs[i...*].toList.sum) ∧
+    (∀ (i : Nat), i ≤ xs.size → s ≤ xs[i...*].toList.sum)
+
+def IsMinSubarraySum (xs : Array Int) (s : Int) : Prop :=
+  if xs = #[] then s = 0 else
+    (∃ (i j : Nat), i < j ∧ j ≤ xs.size ∧ s = xs[i...j].toList.sum) ∧
+      (∀ (i j : Nat), i < j → j ≤ xs.size → s ≤ xs[i...j].toList.sum)
+
+@[simp, grind .]
+theorem isMinSubarraySum₀_empty :
+    IsMinSubarraySum₀ #[] 0 :=
+  ⟨⟨0, 0, by grind, by grind, by grind⟩, fun i j hi hj => by grind⟩
+
+@[simp, grind .]
+theorem isMinSuffixSum₀_empty :
+    IsMinSuffixSum₀ #[] 0 :=
+  ⟨⟨0, by grind, by grind⟩, fun i hi => by grind⟩
+
+@[simp, grind .]
+theorem isMinSubarraySum_empty :
+    IsMinSubarraySum #[] 0 := by
+  grind [IsMinSubarraySum]
+
+@[grind ←, grind →]
+theorem isMinSubarraySum_of_isMinSubarraySum₀_of_neg {xs : Array Int} {s : Int} (hs : s < 0) :
+    IsMinSubarraySum₀ xs s → IsMinSubarraySum xs s := by
+  grind [IsMinSubarraySum, IsMinSubarraySum₀, List.drop_take_self]
+
+theorem isMinSubarraySum_minSubarraySum {xs : Array Int} :
+    IsMinSubarraySum xs (minSubarraySum xs) := by
+  generalize hwp : minSubarraySum xs = w
+  apply Std.Do.Id.of_wp_run_eq hwp
+  mvcgen
+  case inv1 => exact .noThrow fun ⟨cursor, minSum, minSuff⟩ =>
+      ⌜IsMinSubarraySum₀ cursor.prefix.toArray minSum ∧
+        IsMinSuffixSum₀ cursor.prefix.toArray minSuff⌝
+  · mleave
+    simp +zetaDelta
+    sorry -- heavy lifting here
+  · grind
+  · simp only [Array.toArray_toList] at * -- Why? `Array.toArray_toList` is `grind =`-annotated.
+    grind
+  · mleave
+    simp_all
+    sorry
+
 
 /-!
 ## Prompt
