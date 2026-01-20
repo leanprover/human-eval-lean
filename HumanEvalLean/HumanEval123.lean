ba9d689a945170c1fd279a8853fc9efc2cd8588e
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-01-20 10:23:53 +0100

    wip

--- HumanEvalLean/HumanEval123.lean
+++ HumanEvalLean/HumanEval123.lean
@@ -1,5 +1,52 @@
-def get_odd_collatz : Unit :=
-  ()
+import Std
+
+open Std
+
+def collatzStep (n : Nat) : Nat :=
+  if n > 0 then
+    if n % 2 = 0 then n / 2 else n * 3 + 1
+  else
+    1
+
+def CollatzRel : Nat → Nat → Prop := fun m n =>
+    n ≠ 1 ∧ collatzStep n = m
+
+instance : WellFoundedRelation { m : Nat // Acc CollatzRel m } := Acc.wfRel
+
+def oddCollatz (n : Nat) (h : Acc CollatzRel n) : List Nat :=
+  (collectOddCollatz ⟨n, h⟩ ∅).toList
+where
+  -- We attach a proof that `1` is reachable from `n` in finitely many steps to ensure termination.
+  collectOddCollatz (n : { n : Nat // Acc CollatzRel n }) (acc : TreeSet Nat compare) :
+      TreeSet Nat compare :=
+    if h : n.val > 1 then
+      collectOddCollatz ⟨collatzStep n, n.property.inv (by grind [CollatzRel])⟩
+        (if n.val % 2 = 0 then acc else acc.insert n.val)
+    else if n.val = 1 then
+      acc.insert 1
+    else
+      acc
+  termination_by n
+  decreasing_by
+    grind [CollatzRel]
+
+theorem Std.compare_ne_eq [Ord α] [LawfulEqOrd α] {x y : α} :
+    compare x y ≠ .eq ↔ x ≠ y := by
+  simp
+
+instance : LawfulOrderOrd Nat := sorry
+
+theorem oddCollatz_pairwise_distinct {n : Nat} {h : Acc CollatzRel n} :
+    (oddCollatz n h).Pairwise (· ≠ ·) := by
+  simpa [oddCollatz] using TreeSet.distinct_toList (α := Nat) (cmp := compare)
+
+theorem oddCollatz_pairwise_lt {n : Nat} {h : Acc CollatzRel n} :
+    (oddCollatz n h).Pairwise (· < ·) := by
+  simpa [oddCollatz, compare_eq_lt] using TreeSet.ordered_toList (α := Nat) (cmp := compare)
+
+theorem mem_oddCollatz_iff {m n : Nat} {h : Acc CollatzRel n} :
+    m ∈ oddCollatz n h ↔ m = n ∨ CollatzRel m n := by
+  sorry
 
 /-!
 ## Prompt
@@ -11,12 +58,12 @@ def get_odd_collatz(n):
     Given a positive integer n, return a sorted list that has the odd numbers in collatz sequence.
 
     The Collatz conjecture is a conjecture in mathematics that concerns a sequence defined
-    as follows: start with any positive integer n. Then each term is obtained from the 
-    previous term as follows: if the previous term is even, the next term is one half of 
+    as follows: start with any positive integer n. Then each term is obtained from the
+    previous term as follows: if the previous term is even, the next term is one half of
     the previous term. If the previous term is odd, the next term is 3 times the previous
     term plus 1. The conjecture is that no matter what value of n, the sequence will always reach 1.
 
-    Note: 
+    Note:
         1. Collatz(1) is [1].
         2. returned list sorted in increasing order.
 
@@ -29,7 +76,7 @@ def get_odd_collatz(n):
 
 ```python3
     if n%2==0:
-        odd_collatz = [] 
+        odd_collatz = []
     else:
         odd_collatz = [n]
     while n > 1:
@@ -37,7 +84,7 @@ def get_odd_collatz(n):
             n = n/2
         else:
             n = n*3 + 1
-            
+
         if n%2 == 1:
             odd_collatz.append(int(n))
 
