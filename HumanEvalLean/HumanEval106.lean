89de69f58c15484bee59532440717b82fb72168d
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-01-21 11:34:46 +0100

    cleanups

--- HumanEvalLean/HumanEval106.lean
+++ HumanEvalLean/HumanEval106.lean
@@ -3,17 +3,17 @@ module
 import Lean.LibrarySuggestions.Default
 
 import Std.Tactic.Do
--- Sadly, it's apparently currently impossible to easily prove the size of a
--- `Nat` range without unfolding internal stuff. This should be fixed in the standard library.
-import all Init.Data.Iterators.Consumers.Loop
 
 /-!
-This file provides two solutions for problem 106: a naïve one and an efficient one.
+This file provides three solutions for problem 106, progressing from most simple to most efficient.
 -/
 
 set_option mvcgen.warning false
 
 open Std.Do
+
+-- missing grind annotations
+attribute [grind =] Nat.length_toList_rco Std.PRange.Nat.succMany?_eq Nat.not_le
 
 section NaiveImpl
 
@@ -47,10 +47,12 @@ example : f 3 = [1, 2, 6] := by native_decide
 ### Verification
 -/
 
+@[grind =]
 def factorial : Nat → Nat
   | 0 => 1
   | n + 1 => factorial n * (n + 1)
 
+@[grind =]
 def triangle : Nat → Nat
   | 0 => 0
   | n + 1 => triangle n + (n + 1)
@@ -71,7 +73,7 @@ theorem length_f {n : Nat} :
   case inv3 => exact ⇓⟨cur, xs⟩ => ⌜True⌝ -- sum loop
   all_goals simp_all -- relies on `Nat.length_toList_rcc`
 
-theorem f_eq_fac {n : Nat} {k : Nat} (hlt : k < n) :
+theorem getElem_f {n : Nat} {k : Nat} (hlt : k < n) :
     (f n)[k]'(by grind [length_f]) = if k % 2 = 0 then triangle (k + 1) else factorial (k + 1) := by
   rw [List.getElem_eq_iff]
   generalize hwp : f n = w
@@ -150,9 +152,9 @@ theorem size_f' {n : Nat} :
   all_goals try infer_instance
   case inv1 => exact ⇓⟨cur, xs⟩ => ⌜xs.size = cur.prefix.length + 2⌝
   all_goals try (simp_all; done) -- relies on `Nat.size_Rcc`
-  grind [Nat.not_le, Nat.length_toList_rco]
+  grind
 
-theorem f'_eq_fac {n : Nat} {k : Nat} (hlt : k < n) :
+theorem getElem_f' {n : Nat} {k : Nat} (hlt : k < n) :
     (f' n)[k]'(by grind [size_f']) = if k % 2 = 0 then triangle (k + 1) else factorial (k + 1) := by
   rw [Array.getElem_eq_iff]
   generalize hwp : f' n = w
@@ -165,21 +167,21 @@ theorem f'_eq_fac {n : Nat} {k : Nat} (hlt : k < n) :
   case vc1 hn => -- verification of the early return
     -- the return value is a prefix of `[1, 2]` and `k` is the index that needs to be verified
     match k with
-    | 0 => grind [triangle]
-    | 1 => grind [factorial]
+    | 0 => grind
+    | 1 => grind
     | n + 2 => grind
   case vc4 => -- base case of the loop
-    grind [triangle, factorial]
+    grind
   case vc2 hmod h => -- `then` branch
     have := Std.Rco.eq_succMany?_of_toList_eq_append_cons ‹_›
     simp only [Std.PRange.UpwardEnumerable.succMany?] at this
-    grind [factorial]
+    grind
   case vc3 => -- `else` branch
     have := Std.Rco.eq_succMany?_of_toList_eq_append_cons ‹_›
     simp only [Std.PRange.UpwardEnumerable.succMany?] at this
-    grind [triangle]
+    grind
   case vc5 => -- postcondition
-    grind [Nat.not_le, Nat.length_toList_rco]
+    grind
 
 end EfficientImpl
 
@@ -195,15 +197,15 @@ def f'' (n : Nat) : Array Nat := Id.run do
   let mut ret : Array Nat := .emptyWithCapacity n
   ret := ret.push 1 -- 1st entry should be `triangle 1`
   ret := ret.push 2 -- 2nd entry should be `factorial 2`
-  let mut even := 1
-  let mut odd := 2
+  let mut even := 1 -- `triangle 1 = 1`
+  let mut odd := 2 -- `factorial 2 = 2`
   for i in 2...<n do
     if i % 2 = 1 then
-      odd := odd * i * (i + 1)
+      odd := odd * i * (i + 1) -- `factorial (i + 1) = factorial (i - 1) * i * i + 1`
       ret := ret.push odd
     else
-      even := even + 2 * i + 1
-      ret := ret.push (ret[i - 2]! + 2 * i + 1)
+      even := even + 2 * i + 1 -- `triangle (i + 1) = triangle (i - 1) * i * i + 1`
+      ret := ret.push even
   return ret
 
 /-!
@@ -226,13 +228,21 @@ theorem size_f'' {n : Nat} :
   mvcgen
   all_goals try infer_instance
   case inv1 => exact ⇓⟨cur, _, _, xs⟩ => ⌜xs.size = cur.prefix.length + 2⌝
-  all_goals grind [Nat.length_toList_rco]
+  all_goals grind
 
 def lastFactorial (n : Nat) := factorial (n / 2 * 2)
 def lastTriangle (n : Nat) := triangle ((n - 1) / 2 * 2 + 1)
 
 @[grind =] theorem lastTriangle_two : lastTriangle 2 = 1 := by decide
 @[grind =] theorem lastFactorial_two : lastFactorial 2 = 2 := by decide
+
+@[grind →]
+theorem lastTriangle_of_odd (h : n % 2 = 1) : lastTriangle n = triangle n := by
+  grind [lastTriangle]
+
+@[grind →]
+theorem lastFactorial_of_even (h : n % 2 = 0) : lastFactorial n = factorial n := by
+  grind [lastFactorial]
 
 theorem lastTriangle_add_one (h : n % 2 = 1) :
     lastTriangle (n + 1) = lastTriangle n := by
@@ -242,7 +252,7 @@ theorem lastTriangle_add_one (h : n % 2 = 1) :
 theorem lastFactorial_add_one_of_odd (h : n % 2 = 1) :
     lastFactorial (n + 1) = lastFactorial n * n * (n + 1) := by
   have : (n + 1) / 2 * 2 = n / 2 * 2 + 2 := by grind
-  grind [lastFactorial, factorial]
+  grind [lastFactorial]
 
 @[grind =]
 theorem lastTriangle_add_one_of_odd (h : n % 2 = 1) :
@@ -253,16 +263,14 @@ theorem lastTriangle_add_one_of_odd (h : n % 2 = 1) :
 theorem lastTriangle_add_one_of_even (h : n % 2 = 0) (h' : 0 < n) :
     lastTriangle (n + 1) = lastTriangle n + 2 * n + 1 := by
   have : n / 2 * 2 = (n - 1) / 2 * 2 + 2 := by grind
-  grind [lastTriangle, triangle]
+  grind [lastTriangle]
 
 @[grind =]
 theorem lastFactorial_add_one_of_even (h : n % 2 = 0) :
     lastFactorial (n + 1) = lastFactorial n := by
   grind [lastFactorial]
 
-attribute [grind =] Std.PRange.Nat.succMany?_eq
-
-theorem f''_eq_fac {n : Nat} {k : Nat} (hlt : k < n) :
+theorem getElem_f'' {n : Nat} {k : Nat} (hlt : k < n) :
     (f'' n)[k]'(by grind [size_f'']) = if k % 2 = 0 then triangle (k + 1) else factorial (k + 1) := by
   rw [Array.getElem_eq_iff]
   generalize hwp : f'' n = w
@@ -275,19 +283,19 @@ theorem f''_eq_fac {n : Nat} {k : Nat} (hlt : k < n) :
   case vc1 hn => -- verification of the early return
     -- the return value is a prefix of `[1, 2]` and `k` is the index that needs to be verified
     match k with
-    | 0 => grind [triangle]
-    | 1 => grind [factorial]
+    | 0 => grind
+    | 1 => grind
     | n + 2 => grind
   case vc4 => -- base case of the loop
-    grind [triangle, factorial]
+    grind
   case vc2 hmod h => -- `then` branch
     have := Std.Rco.eq_succMany?_of_toList_eq_append_cons ‹_›
-    grind [lastFactorial, factorial]
+    grind
   case vc3 => -- `else` branch
     have := Std.Rco.eq_succMany?_of_toList_eq_append_cons ‹_›
-    grind [triangle]
+    grind
   case vc5 => -- postcondition
-    grind [Nat.not_le, Nat.length_toList_rco]
+    grind
 
 end MoreEfficientImpl
 
