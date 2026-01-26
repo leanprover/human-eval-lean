fdb0126b4ad125ccbae0f8703518d10328865654
Syed Shaazib Tanvir <143099215+shaazib-tanvir@users.noreply.github.com>
2026-01-26 13:18:01 +0530

    HumanEval8 (#206)
    This PR solves problem 8.

--- HumanEvalLean/HumanEval8.lean
+++ HumanEvalLean/HumanEval8.lean
@@ -1,5 +1,63 @@
-def sum_product : Unit :=
-  ()
+import Std.Tactic.Do
+open Std.Do
+set_option mvcgen.warning false
+
+@[grind]
+def List.product (xs : List Int) : Int :=
+  match xs with
+  | [] => 1
+  | x :: xs' => x * (product xs')
+
+@[grind]
+def sumProduct (xs : List Int) : Int × Int := (List.sum xs, List.product xs)
+
+def sumProductDo (xs : List Int) : Int × Int := Id.run do
+  let mut sum := 0
+  let mut product := 1
+  for x in xs do
+    sum := sum + x
+    product := product * x
+
+  return (sum, product)
+
+@[grind =]
+theorem List.sum_append (xs : List Int) (ys : List Int) :
+    sum (xs ++ ys) = sum xs + sum ys := by
+  induction xs with
+  | nil =>
+    change sum ys = 0 + sum ys
+    omega
+  | cons x xs hi =>
+    change x + sum (xs ++ ys) = x + sum xs + sum ys
+    rw [hi]
+    omega
+
+@[grind =]
+theorem List.product_append (xs : List Int) (ys : List Int) :
+    product (xs ++ ys) = product xs * product ys := by
+  induction xs with
+  | nil =>
+    change product ys = 1 * product ys
+    omega
+  | cons x xs hi =>
+    change x * product (xs ++ ys) = x * product xs * product ys
+    rw [hi]
+    simp only [Int.mul_assoc]
+
+theorem sumProductDo_correct (xs : List Int) : sumProductDo xs = sumProduct xs := by
+  generalize h : sumProductDo xs = ys
+  apply Id.of_wp_run_eq h
+  mvcgen
+  invariants
+  · ⇓ ⟨ cur, result ⟩ =>
+    ⌜ result.snd = List.sum cur.prefix ∧ result.fst = List.product cur.prefix ⌝
+  with grind
+
+example : sumProductDo [] = (0, 1) := by rfl
+example : sumProductDo [1, 1, 1] = (3, 1) := by rfl
+example : sumProductDo [100, 0] = (100, 0) := by rfl
+example : sumProductDo [3, 5, 7] = (3 + 5 + 7, 3 * 5 * 7) := by rfl
+example : sumProductDo [10] = (10, 10) := by rfl
 
 /-!
 ## Prompt
