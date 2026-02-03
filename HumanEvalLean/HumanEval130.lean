714f512b31625dc1c64ca6511896ee177732bdf5
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-02-03 15:17:24 +0100

    iterator solution

--- HumanEvalLean/HumanEval130.lean
+++ HumanEvalLean/HumanEval130.lean
@@ -4,35 +4,31 @@ open Std Std.Do
 
 set_option mvcgen.warning false
 
-/-!
-## Implementation
--/
+/-! ## Implementation using iterators -/
 
 /--
 The Tribonacci sequence has an unusual recurrence for odd indices:
-`tri(n) = tri(n-1) + tri(n-2) + tri(n+1)` when `n ≥ 2` is odd.
+`tri'(n) = tri'(n-1) + tri'(n-2) + tri'(n+1)` when `n ≥ 2` is odd.
 
 This creates a forward reference, but since `n + 1` is even, we can substitute
-`tri(n + 1) = 1 + (n + 1) / 2 = (n + 3) / 2`, giving us a recurrence only depending on predecessors:
+`tri'(n + 1) = 1 + (n + 1) / 2 = (n + 3) / 2`, giving us a recurrence only depending on predecessors:
 
-`tri(n) = tri(n - 1) + tri(n - 2) + (n + 3) / 2` when `n ≥ 2` is odd.
+`tri'(n) = tri'(n - 1) + tri'(n - 2) + (n + 3) / 2` when `n ≥ 2` is odd.
 -/
-def tri (n : Nat) : List Nat := Id.run do
-  let mut xs : Array Nat := #[]
-  let mut lastₑ := 1
-  let mut lastₒ := 0
-  for i in 0...=n do
-    if i % 2 == 0 then
-      lastₑ := i / 2 + 1
-      xs := xs.push lastₑ
-    else
-      lastₒ := (lastₑ + lastₒ + (i + 3) / 2)
-      xs := xs.push lastₒ
-  return xs.toList
+@[inline]
+def tribonacciIterator.step : Nat × Nat × Nat → Nat × Nat × Nat
+  | (i, beforeLast, last) => (i + 1, last,
+      if i % 2 = 0 then i / 2 + 1 else beforeLast + last + (i + 3) / 2)
 
-/-!
-## Tests
--/
+@[inline]
+def tribonacciIterator.states := Iter.repeat (init := (1, 0, 1)) tribonacciIterator.step
+
+def tribonacciIterator := tribonacciIterator.states.map (fun (_, _, current) => current)
+
+def tri (n : Nat) : List Nat :=
+  tribonacciIterator.take (n + 1) |>.toList
+
+/-! ## Tests -/
 
 example : tri 3 = [1, 3, 2, 8] := by native_decide
 example : tri 4 = [1, 3, 2, 8, 3] := by native_decide
@@ -41,13 +37,12 @@ example : tri 6 = [1, 3, 2, 8, 3, 15, 4] := by native_decide
 example : tri 7 = [1, 3, 2, 8, 3, 15, 4, 24] := by native_decide
 example : tri 8 = [1, 3, 2, 8, 3, 15, 4, 24, 5] := by native_decide
 example : tri 9 = [1, 3, 2, 8, 3, 15, 4, 24, 5, 35] := by native_decide
-example : tri 20 = [1, 3, 2, 8, 3, 15, 4, 24, 5, 35, 6, 48, 7, 63, 8, 80, 9, 99, 10, 120, 11] := by native_decide
+example : tri 20 = [1, 3, 2, 8, 3, 15, 4, 24, 5, 35, 6, 48, 7, 63, 8, 80, 9, 99, 10, 120, 11] := by
+  native_decide
 example : tri 0 = [1] := by native_decide
 example : tri 1 = [1, 3] := by native_decide
 
-/-!
-## Missing API
--/
+/-! ## Missing API -/
 
 theorem Nat.eq_add_of_toList_rcc_eq_append_cons {a b : Nat} {pref cur suff}
     (h : (a...=b).toList = pref ++ cur :: suff) :
@@ -55,7 +50,107 @@ theorem Nat.eq_add_of_toList_rcc_eq_append_cons {a b : Nat} {pref cur suff}
   have := Rcc.eq_succMany?_of_toList_eq_append_cons h
   grind [PRange.Nat.succMany?_eq]
 
-/-! ## Verification -/
+@[grind =]
+theorem Std.Iter.atIdxSlow?_map [Iterator α Id β] [Iterators.Productive α Id]
+    {it : Iter (α := α) β} {f : β → γ} :
+    (it.map f).atIdxSlow? n = (it.atIdxSlow? n).map f := by
+  induction n, it using atIdxSlow?.induct_unfolding
+  all_goals
+    rw [atIdxSlow?_eq_match, step_map]
+    simp [*]
+
+attribute [grind =] Iter.atIdxSlow?_repeat
+
+theorem Std.Iter.length_take_repeat {f : α → α} {init} :
+    (Iter.repeat (init := init) f |>.take n).length = n := by
+  induction n generalizing init <;>
+    grind [=_ Iter.length_toList_eq_length, Iter.toList_take_zero, Iter.toList_take_repeat_succ]
+
+theorem Std.Iter.toList_take_map [Iterator α Id β] [Iterators.Productive α Id]
+    {it : Iter (α := α) β} {f : β → γ} :
+    (it.map f |>.take n).toList = (it.take n).toList.map f := by
+  apply List.ext_getElem?
+  grind [Iter.getElem?_toList_eq_atIdxSlow?, Iter.atIdxSlow?_take]
+
+attribute [grind =] Iter.atIdxSlow?_take Iter.toList_take_map Iter.length_map
+  Iter.length_toList_eq_length Iter.length_take_repeat
+
+/-! ## Verification of `tri` -/
+
+def tribonacci (n : Nat) :=
+  (tribonacciIterator.atIdxSlow? n).get (by grind [tribonacciIterator, tribonacciIterator.states])
+
+def tribonacciState (n : Nat) :=
+  (tribonacciIterator.states.atIdxSlow? n).get (by grind [tribonacciIterator.states])
+
+theorem tribonacci_eq_tribonacciState :
+    tribonacci n = (tribonacciState n).2.2 := by
+  grind [tribonacci, tribonacciState, tribonacciIterator]
+
+theorem tribonacciState_spec :
+    tribonacciState n =
+      if n = 0 then
+        (1, 0, 1)
+      else if n = 1 then
+        (2, 1, 3)
+      else if n % 2 = 0 then
+        (n + 1, tribonacci (n - 1), 1 + n / 2)
+      else
+        (n + 1, tribonacci (n - 1), tribonacci (n - 2) + tribonacci (n - 1) + (n + 3) / 2) := by
+  simp only [tribonacci, tribonacciState, tribonacciIterator, tribonacciIterator.states]
+  induction n using Nat.strongRecOn with | ind n ih
+  match n with
+  | 0 | 1 | n + 2 => grind [Nat.repeat, tribonacciIterator.step]
+
+theorem tribonacci_spec:
+    tribonacci n =
+      if n = 1 then
+        3
+      else if n % 2 = 0 then
+        1 + n / 2
+      else
+        tribonacci (n - 2) + tribonacci (n - 1) + tribonacci (n + 1) := by
+  match n with
+  | 0 | 1 | n + 2 => grind [tribonacciState_spec, tribonacci_eq_tribonacciState]
+
+@[grind =]
+theorem length_tri :
+    (tri n).length = n + 1 := by
+  grind [tri, tribonacciIterator, tribonacciIterator.states]
+
+theorem tri_spec (h : i ≤ n) :
+    (tri n)[i]'(by grind) = tribonacci i := by
+  grind [tri, tribonacci, tribonacciIterator, Iter.getElem?_toList_eq_atIdxSlow?]
+
+/-! ## Alternative implementation using `do` notation -/
+
+def tri' (n : Nat) : List Nat := Id.run do
+  let mut xs : Array Nat := #[]
+  let mut lastₑ := 1
+  let mut lastₒ := 0
+  for i in 0...=n do
+    if i % 2 = 0 then
+      lastₑ := i / 2 + 1
+      xs := xs.push lastₑ
+    else
+      lastₒ := (lastₑ + lastₒ + (i + 3) / 2)
+      xs := xs.push lastₒ
+  return xs.toList
+
+/-! ## Tests -/
+
+example : tri' 3 = [1, 3, 2, 8] := by native_decide
+example : tri' 4 = [1, 3, 2, 8, 3] := by native_decide
+example : tri' 5 = [1, 3, 2, 8, 3, 15] := by native_decide
+example : tri' 6 = [1, 3, 2, 8, 3, 15, 4] := by native_decide
+example : tri' 7 = [1, 3, 2, 8, 3, 15, 4, 24] := by native_decide
+example : tri' 8 = [1, 3, 2, 8, 3, 15, 4, 24, 5] := by native_decide
+example : tri' 9 = [1, 3, 2, 8, 3, 15, 4, 24, 5, 35] := by native_decide
+example : tri' 20 = [1, 3, 2, 8, 3, 15, 4, 24, 5, 35, 6, 48, 7, 63, 8, 80, 9, 99, 10, 120, 11] := by native_decide
+example : tri' 0 = [1] := by native_decide
+example : tri' 1 = [1, 3] := by native_decide
+
+/-! ## Verification of `tri'` -/
 
 /-! ### Loop invariants -/
 
@@ -79,12 +174,12 @@ def Inv₅ (xs : Array Nat) : Prop :=
 /-!
 ### Basic correctness theorems
 
-We start by verifying that the elements of `tri n` satisfy a recurrence relation.
+We start by verifying that the elements of `tri' n` satisfy a recurrence relation.
 -/
 
 /-- Auxiliary lemma, corollaries with simpler statements follow afterwards. -/
-theorem tri_recurrence {n i} {h : i ≤ n} :
-    (tri n)[i]! =
+theorem tri'_recurrence {n i} {h : i ≤ n} :
+    (tri' n)[i]! =
       if i = 0 then
         1
       else if i = 1 then
@@ -92,9 +187,9 @@ theorem tri_recurrence {n i} {h : i ≤ n} :
       else if i % 2 = 0 then
         1 + i / 2
       else
-        (tri n)[i - 2]! + (tri n)[i - 1]! + (i + 3) / 2 := by
-  let wp := tri n
-  have hwp : tri n = wp := rfl
+        (tri' n)[i - 2]! + (tri' n)[i - 1]! + (i + 3) / 2 := by
+  let wp := tri' n
+  have hwp : tri' n = wp := rfl
   simp only [hwp]
   clear_value wp
   apply Id.of_wp_run_eq hwp
@@ -160,74 +255,74 @@ theorem tri_recurrence {n i} {h : i ≤ n} :
 
 /-! ### Main theorems -/
 
-/-- `tri n` is a list with `n + 1` elements. -/
+/-- `tri' n` is a list with `n + 1` elements. -/
 @[simp, grind =]
-theorem length_tri : (tri n).length = n + 1 := by
-  generalize hwp : tri n = wp
+theorem length_tri' : (tri' n).length = n + 1 := by
+  generalize hwp : tri' n = wp
   apply Id.of_wp_run_eq hwp
   mvcgen
   invariants
   · ⇓⟨cur, lastₑ, lastₒ, xs⟩ => ⌜xs.size = cur.pos⌝
   with grind [Nat.length_toList_rcc]
 
-/-- The zero-th value is `1`. This would also follow from `tri_of_even`. -/
-theorem tri_zero :
-    (tri n)[0] = 1 := by
-  have := tri_recurrence (n := n) (i := 0) (h := by grind)
+/-- The zero-th value is `1`. This would also follow from `tri'_of_even`. -/
+theorem tri'_zero :
+    (tri' n)[0] = 1 := by
+  have := tri'_recurrence (n := n) (i := 0) (h := by grind)
   grind
 
 /-- The first value is `3`. -/
-theorem tri_one (h : 1 ≤ n) :
-    (tri n)[1]'(by grind) = 3 := by
-  have := tri_recurrence (n := n) (i := 1) (h := h)
+theorem tri'_one (h : 1 ≤ n) :
+    (tri' n)[1]'(by grind) = 3 := by
+  have := tri'_recurrence (n := n) (i := 1) (h := h)
   grind
 
 /-- The value at even position `i` is `1 + (i / 2)`. -/
-theorem tri_of_even (h : i ≤ n) (hi : i % 2 = 0) :
-    (tri n)[i]'(by grind) = 1 + (i / 2) := by
-  have := tri_recurrence (n := n) (i := i) (h := h)
+theorem tri'_of_even (h : i ≤ n) (hi : i % 2 = 0) :
+    (tri' n)[i]'(by grind) = 1 + (i / 2) := by
+  have := tri'_recurrence (n := n) (i := i) (h := h)
   grind
 
 /--
 The value at odd position `i ≥ 2` is the sum of its two predecessors plus `(i + 3) / 2`.
 
-The next lemma, `tri_of_odd₂`, will prove the exact equation for odd positions provided by the
-problem description, which expresses `tri(i)` in terms of `tri(i - 2)`, `tri(i - 1)` and
-`tri(i + 1)`. Therefore, `tri-of_odd₂` needs to assume `i + 1 ≤ n` in order for `(tri n)[i + 1]` to
-exist. In contrast, the given lemma does not rely on `(tri n)[i + 1]`, making it also applicable for
+The next lemma, `tri'_of_odd₂`, will prove the exact equation for odd positions provided by the
+problem description, which expresses `tri'(i)` in terms of `tri'(i - 2)`, `tri'(i - 1)` and
+`tri'(i + 1)`. Therefore, `tri'-of_odd₂` needs to assume `i + 1 ≤ n` in order for `(tri' n)[i + 1]` to
+exist. In contrast, the given lemma does not rely on `(tri' n)[i + 1]`, making it also applicable for
 `i ≤ n`.
 -/
-theorem tri_of_odd₁ (h : i ≤ n) (hge : 2 ≤ i) (hi : i % 2 = 1) :
-    (tri n)[i]'(by grind) = (tri n)[i - 2]'(by grind) + (tri n)[i - 1]'(by grind) + (i + 3) / 2 := by
-  have := tri_recurrence (n := n) (i := i) (h := h)
+theorem tri'_of_odd₁ (h : i ≤ n) (hge : 2 ≤ i) (hi : i % 2 = 1) :
+    (tri' n)[i]'(by grind) = (tri' n)[i - 2]'(by grind) + (tri' n)[i - 1]'(by grind) + (i + 3) / 2 := by
+  have := tri'_recurrence (n := n) (i := i) (h := h)
   grind
 
 /--
 This is the property specified for the value at even positions:
 The value at even position `i ≥ 2` is the sum of its two predecessors and its immediate successor.
 -/
-theorem tri_of_odd₂ (h : i + 1 ≤ n) (hge : 2 ≤ i) (hi : i % 2 = 1) :
-    (tri n)[i]'(by grind) = (tri n)[i - 2]'(by grind) + (tri n)[i - 1]'(by grind) + (tri n)[i + 1]'(by grind) := by
-  grind [tri_of_odd₁, tri_of_even]
+theorem tri'_of_odd₂ (h : i + 1 ≤ n) (hge : 2 ≤ i) (hi : i % 2 = 1) :
+    (tri' n)[i]'(by grind) = (tri' n)[i - 2]'(by grind) + (tri' n)[i - 1]'(by grind) + (tri' n)[i + 1]'(by grind) := by
+  grind [tri'_of_odd₁, tri'_of_even]
 
 /--
-If `m ≤ n`, then `tri m` is a prefix of `tri n`.
+If `m ≤ n`, then `tri' m` is a prefix of `tri' n`.
 
-This proves that `tri` always return a prefix of the very same infinite sequence.
-Together with the lemmas `tri_one`, `tri_of_even` and `tri_of_odd₂`, it follows that this sequence
+This proves that `tri'` always return a prefix of the very same infinite sequence.
+Together with the lemmas `tri'_one`, `tri'_of_even` and `tri'_of_odd₂`, it follows that this sequence
 obeys the laws of the tribonacci sequence, as stated in the problem description.
 -/
-theorem prefix_tri_of_le (h : m ≤ n) :
-    tri m <+: tri n := by
+theorem prefix_tri'_of_le (h : m ≤ n) :
+    tri' m <+: tri' n := by
   simp only [List.IsPrefix]
-  refine ⟨(tri n).drop (m + 1), ?_⟩
+  refine ⟨(tri' n).drop (m + 1), ?_⟩
   apply List.ext_getElem
   · grind
   · intro i _ _
     by_cases i ≤ m
     · rw [List.getElem_append_left (by grind)]
       induction i using Nat.strongRecOn with | ind i ih
-      grind [tri_of_even, tri_of_odd₁, tri_one]
+      grind [tri'_of_even, tri'_of_odd₁, tri'_one]
     · grind
 
 /-!
@@ -235,22 +330,22 @@ theorem prefix_tri_of_le (h : m ≤ n) :
 
 ```python3
 
-def tri(n):
+def tri'(n):
     """Everyone knows Fibonacci sequence, it was studied deeply by mathematicians in
     the last couple centuries. However, what people don't know is Tribonacci sequence.
     Tribonacci sequence is defined by the recurrence:
-    tri(1) = 3
-    tri(n) = 1 + n / 2, if n is even.
-    tri(n) =  tri(n - 1) + tri(n - 2) + tri(n + 1), if n is odd.
+    tri'(1) = 3
+    tri'(n) = 1 + n / 2, if n is even.
+    tri'(n) =  tri'(n - 1) + tri'(n - 2) + tri'(n + 1), if n is odd.
     For example:
-    tri(2) = 1 + (2 / 2) = 2
-    tri(4) = 3
-    tri(3) = tri(2) + tri(1) + tri(4)
+    tri'(2) = 1 + (2 / 2) = 2
+    tri'(4) = 3
+    tri'(3) = tri'(2) + tri'(1) + tri'(4)
            = 2 + 3 + 3 = 8
     You are given a non-negative integer number n, you have to a return a list of the
     first n + 1 numbers of the Tribonacci sequence.
     Examples:
-    tri(3) = [1, 3, 2, 8]
+    tri'(3) = [1, 3, 2, 8]
     """
 ```
 
