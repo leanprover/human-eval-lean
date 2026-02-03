d9cd8118be6d3090f14c08c7d4bea8cbbb45b00a
Paul Reichert <6992158+datokrat@users.noreply.github.com>
2026-02-03 19:05:00 +0100

    133 (#225)
    This PR solves problem 133

--- HumanEvalLean/HumanEval133.lean
+++ HumanEvalLean/HumanEval133.lean
@@ -1,5 +1,107 @@
-def sum_squares : Unit :=
-  ()
+
+def sumSquares (xs : List Rat) : Int :=
+  xs.map (·.ceil ^ (2 : Nat)) |>.sum
+
+/-! ## Tests -/
+
+example : sumSquares [1, 2, 3] = 14 := by native_decide
+example : sumSquares [1.0, 2, 3] = 14 := by native_decide
+example : sumSquares [1, 3, 5, 7] = 84 := by native_decide
+example : sumSquares [1.4, 4.2, 0] = 29 := by native_decide
+example : sumSquares [-2.4, 1, 1] = 6 := by native_decide
+example : sumSquares [100, 1, 15, 2] = 10230 := by native_decide
+example : sumSquares [10000, 10000] = 200000000 := by native_decide
+example : sumSquares [-1.4, 4.6, 6.3] = 75 := by native_decide
+example : sumSquares [-1.4, 17.9, 18.9, 19.9] = 1086 := by native_decide
+example : sumSquares [0] = 0 := by native_decide
+example : sumSquares [-1] = 1 := by native_decide
+example : sumSquares [-1, 1, 0] = 2 := by native_decide
+
+/-! ## Missing API -/
+
+theorem Int.neg_le_iff {x y : Int} :
+    - x ≤ y ↔ - y ≤ x := by
+  grind
+
+theorem Int.lt_neg_iff {x y : Int} :
+    x < - y ↔ y < - x := by
+  grind
+
+theorem Rat.le_neg_iff {x y : Rat} :
+    x ≤ - y ↔ y ≤ - x := by
+  grind
+
+theorem Rat.add_lt_add_iff_right (c : Rat) :
+    a + c < b + c ↔ a < b := by
+  grind
+
+theorem Rat.lt_iff_le_not_le {x y : Rat} :
+    x < y ↔ x ≤ y ∧ ¬ y ≤ x := by
+  grind
+
+theorem Rat.ceil_le_iff {x : Rat} {y : Int} :
+    x.ceil ≤ y ↔ x ≤ y := by
+  simp only [Rat.ceil_eq_neg_floor_neg, Int.neg_le_iff, Rat.le_floor_iff]
+  grind
+
+theorem Rat.lt_ceil_iff {x : Rat} {y : Int} :
+    y < x.ceil ↔ y < x := by
+  simp only [Rat.ceil_eq_neg_floor_neg, Int.lt_neg_iff, Rat.floor_lt_iff]
+  grind
+
+theorem Rat.le_ceil {x : Rat} :
+    x ≤ x.ceil := by
+  simp only [Rat.ceil_eq_neg_floor_neg, Rat.intCast_neg, Rat.le_neg_iff, Rat.floor_le]
+
+theorem Rat.ceil_le_intCast {x : Rat} {y : Int} :
+    (x + y).ceil ≤ x.ceil + y := by
+  simp only [Rat.ceil_le_iff]
+  grind [Rat.le_ceil]
+
+theorem Rat.ceil_add_intCast {x : Rat} {y : Int} :
+    (x + y).ceil = x.ceil + y := by
+  apply Std.le_antisymm
+  · apply ceil_le_intCast
+  · have := ceil_le_intCast (x := x + y) (y := - y)
+    grind
+
+theorem Rat.ceil_add_one {x : Rat} :
+    (x + 1).ceil = x.ceil + 1 := by
+  have := ceil_add_intCast (x := x) (y := 1)
+  grind
+
+theorem Rat.ceil_lt {x : Rat} :
+    x.ceil < x + 1 := by
+  rw [← Rat.lt_ceil_iff, Rat.ceil_add_one]
+  grind
+
+/-!
+## Verification
+
+We start pointing to lemmas that verify `Rat.ceil` and then express the correctness lemmas in
+terms of `Rat.ceil`.
+-/
+
+/-- info: Rat.ceil_lt {x : Rat} : ↑x.ceil < x + 1 -/
+#guard_msgs in
+#check Rat.ceil_lt
+
+/-- info: Rat.le_ceil {x : Rat} : x ≤ ↑x.ceil -/
+#guard_msgs in
+#check Rat.le_ceil
+
+@[grind =]
+theorem sumSquares_nil :
+    sumSquares [] = 0 := by
+  grind [sumSquares]
+
+theorem sumSquares_singleton :
+    sumSquares [x] = x.ceil * x.ceil := by
+  grind [sumSquares]
+
+theorem sumSquares_append {xs ys : List Rat} :
+    sumSquares (xs ++ ys) = sumSquares xs + sumSquares ys := by
+  grind [sumSquares]
 
 /-!
 ## Prompt
@@ -17,7 +119,7 @@ def sum_squares(lst):
     For lst = [1,3,5,7] the output should be 84
     For lst = [1.4,4.2,0] the output should be 29
     For lst = [-2.4,1,1] the output should be 6
-    
+
 
     """
 ```
