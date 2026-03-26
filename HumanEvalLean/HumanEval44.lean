b95a721767846ec19efaafc8202423554fa29c57
Markus Himmel <2065352+TwoFX@users.noreply.github.com>
2026-03-26 09:23:05 +0100

    44 (#295)

--- HumanEvalLean/HumanEval44.lean
+++ HumanEvalLean/HumanEval44.lean
@@ -1,7 +1,18 @@
 module
 
-def change_base : Unit :=
-  ()
+def changeBase (x : Nat) (base : Nat) : String :=
+  String.ofList (Nat.toDigits base x)
+
+example : changeBase 8 3 = "22" := by native_decide
+example : changeBase 9 3 = "100" := by native_decide
+example : changeBase 234 2 = "11101010" := by native_decide
+example : changeBase 16 2 = "10000" := by native_decide
+example : changeBase 8 2 = "1000" := by native_decide
+example : changeBase 7 2 = "111" := by native_decide
+
+theorem ofDigitChars_changeBase {x : Nat} {base : Nat} (hb' : 1 < base) (hb : base ≤ 10) :
+    Nat.ofDigitChars base (changeBase x base).toList 0 = x := by
+  simp [changeBase, Nat.ofDigitChars_toDigits hb' hb]
 
 /-!
 ## Prompt
